b1e896f993a27dd20629f55ec814d6d0007c73c6
NewwhatsAppchatebot <146094575+NewwhatsAppchatebot@users.noreply.github.com>
2024-06-30 19:05:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ module.exports = {
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "𝐅𝐈𝐋𝐄-𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃-𝐁𝐘-Faheem-hacker-𝐕2😍",
-  author: process.env.PACK_AUTHER || "WASO",
+  author: process.env.PACK_AUTHER || "Faheem",
   packname: process.env.PACK_NAME || "♥️",
   botname: process.env.BOT_NAME || "𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2",
   ownername: process.env.OWNER_NAME || "Faheem",
